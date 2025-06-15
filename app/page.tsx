390c6984d11e92817e35cd33f533da0ffb7a9285
Patrick MacDonald <patrick@patmac.ca>
2025-06-14 20:14:54 -0600

    fixed build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { CreateOrganization, OrganizationProfile, OrganizationSwitcher } from "@clerk/nextjs";
+// import { OrganizationProfile } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default async function Home() {
-  const { userId, sessionClaims } = await auth()
+  const { userId, sessionClaims, has } = await auth()
 
   // Protect the route by checking if the user is signed in
   if (!userId) {
@@ -14,12 +14,16 @@ export default async function Home() {
   const user = await currentUser()
   console.log(sessionClaims)
 
+  console.log("user has plan: ", has({ plan: 'Basic 10 people org' }))
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>Welcome, {user?.firstName}!</div>
-      <CreateOrganization />
-      
-      {/* <OrganizationProfile /> */}
+      {/* <CreateOrganization /> */}
+      {/* <PricingTable routing="hash"/> */}
+      {/* <UserProfile routing="hash"/>  */}
+
+      {/* <OrganizationProfile  routing="hash"/> */}
     </div>
   );
 }
